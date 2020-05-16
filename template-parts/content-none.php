50cadd61599398e8eb2c72f7fbd325ffe37eb526
Lucia <luciasanchezfraile@gmail.com>
2020-05-16 13:02:45 +0200

    no content template part added

---
+++ template-parts/content-none.php
@@ -0,0 +1,10 @@
+<?php 
+/**
+ * Template part to display when there is no content
+ * 
+ * @package eats
+ */
+
+?>
+
+<p class="page-title"><?php esc_html_e( 'Sorry, no post yet!', 'eats' ); ?></p>
