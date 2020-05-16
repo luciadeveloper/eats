31f63eae6f2d3ba8639cabf95fe89df52f775b1b
Lucia <luciasanchezfraile@gmail.com>
2020-05-16 12:45:18 +0200

    header added

--- index.php
+++ index.php
@@ -1,19 +1,28 @@
 <?php 
 get_header();
-
 ?>
-<?php
-    if ( have_posts() ) :
-        while ( have_posts() ) :
-            the_post();
 
-            get_template_part( 'template-parts/content', get_post_type() );
+<main id="primary" class="site-main container"  role="main">
 
-        endwhile;
+    <?php
+        if ( have_posts() ) :
+        
+            while ( have_posts() ) :
+                the_post();
 
-    else :
+                get_template_part( 'template-parts/content', get_post_type() );
 
-        get_template_part( 'template-parts/content', 'none' );
+            endwhile;
 
-    endif;
+        else :
+
+            get_template_part( 'template-parts/content', 'none' );
+
+        endif;
+    ?>
+
+</main>
+
+<?php 
+get_footer();
 ?>
