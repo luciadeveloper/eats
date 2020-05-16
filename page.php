7e6e1d0f76616d6145baf49e3640120b7863adb8
Lucia <luciasanchezfraile@gmail.com>
2020-05-16 16:08:06 +0200

    template added

---
+++ page.php
@@ -0,0 +1,28 @@
+<?php 
+get_header();
+?>
+
+<main id="primary" class="site-main container"  role="main">
+
+    <?php
+        if ( have_posts() ) :
+        
+            while ( have_posts() ) :
+                the_post();
+
+                get_template_part( 'template-parts/content');
+
+            endwhile;
+
+        else :
+
+            get_template_part( 'template-parts/content', 'none' );
+
+        endif;
+    ?>
+
+</main>
+
+<?php 
+get_footer();
+?>
