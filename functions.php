5aa75256bc336fca1d2def97390c04dc9528e2ad
Lucia <luciasanchezfraile@gmail.com>
2020-05-16 16:10:18 +0200

    theme version added

--- functions.php
+++ functions.php
@@ -6,15 +6,17 @@
  */
 
 
+if ( ! defined( 'THEME_VERSION' ) ) {
+	// Replace the version number of the theme on each release.
+	define( 'THEME_VERSION', '1.0.0' );
+}
+
+
 
 /**
  * Theme setup.
  */
 
-if ( ! defined( '_THEME_VERSION' ) ) {
-	// Replace the version number of the theme on each release.
-	define( '_THEME_VERSION', '1.0.0' );
-}
 
 if ( ! function_exists( 'eats_setup' ) ) {
 
@@ -41,10 +43,10 @@ add_image_size('blog-thumbnail', 680, 380);
  * Enqueue scripts and styles.
  */
 function eats_scripts() {
-	wp_enqueue_style( 'eats-style', get_stylesheet_uri(), array(), _THEME_VERSION );
-	
-	wp_enqueue_script( 'eats-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _THEME_VERSION, true );
 
-	
+    wp_enqueue_style( 'eats-style', get_stylesheet_uri(), array(), THEME_VERSION );
+   
+    wp_enqueue_script( 'script-name', get_template_directory_uri() . '/js/navigation.js', array(), THEME_VERSION, true );
+    
 }
 add_action( 'wp_enqueue_scripts', 'eats_scripts' );
