a2c0cbfd0cd0fe22151ae59a8cb74ef4038775b6
Lucia <luciasanchezfraile@gmail.com>
2020-05-16 10:42:14 +0200

    functions theme setup

---
+++ functions.php
@@ -0,0 +1,38 @@
+<?php 
+
+/**
+ * Theme setup.
+ */
+
+if ( ! function_exists( 'eats_setup' ) ) {
+
+    function eats_setup() {
+       
+        add_theme_support( 'title-tag' );
+        add_theme_support( 'post-thumbnails' );
+
+        register_nav_menus(
+            array(
+                'menu-1' => esc_html__( 'Primary', 'eats' ),
+            )
+        );
+    }
+
+}
+
+add_action( 'after_setup_theme', 'eats_setup' );
+
+add_image_size('blog-thumbnail', 680, 380);
+
+
+/**
+ * Enqueue scripts and styles.
+ */
+function eats_scripts() {
+	wp_enqueue_style( 'eats-style', get_stylesheet_uri(), array(), _S_VERSION );
+	
+	wp_enqueue_script( 'eats-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+
+	
+}
+add_action( 'wp_enqueue_scripts', 'eats_scripts' );
